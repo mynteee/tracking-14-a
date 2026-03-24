b42c51ed8ce101478ccf21845787a1c815e1695e
Sarvesh-Mk <me.fast.learn@gmail.com>
2026-03-24 15:21:21 -0400

    storing which tracker is associated with a node

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,16 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Image } from 'expo-image';
 import Paho from "paho-mqtt";
 import { useEffect, useState } from "react";
 import { View } from "react-native";
+
+/*
+I have no idea what Im doing I also didn't comment so lowk kinda cooked
+*/
+
+const blurhash = '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+const rooms = ["room_1", "room_2", "room_3", "room_4", "room_5"];
+
 
 function onConnectionLost(responseObject: any) {
   if (responseObject.errorCode !== 0) {
@@ -9,8 +18,11 @@ function onConnectionLost(responseObject: any) {
   }
 }
 
-const blurhash = '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
-
+async function getDataFromRoom(room: string) {
+  const value = await AsyncStorage.getItem(room);
+  console.log(`Data for ${room}:`, value);
+  return value;
+}
 
 export default function Index() {
   const [data, setData] = useState("Waiting for data...");
@@ -21,10 +33,19 @@ export default function Index() {
     const client = new Paho.Client("172.20.10.2", 9001, `client-${Date.now()}`);
     client.onConnectionLost = onConnectionLost;
     
-    client.onMessageArrived = (message: any) => {
+    /*
+    very impt but im too lazy to explain it
+    */
+    client.onMessageArrived = async (message: any) => {
       console.log("Topic:", message.destinationName);
       console.log("Payload:", message.payloadString);
-      setData(`${message.destinationName}: ${message.payloadString}`);
+      const payload = JSON.parse(message.payloadString)["known"];
+      const room = message.destinationName.split("/")[-1];
+      await AsyncStorage.setItem(room, payload);
+      setData("");
+      for (const r of rooms) {
+        setData(`${data}${r}: ${await getDataFromRoom(r)},`);
+      }
     };
 
     client.connect({
